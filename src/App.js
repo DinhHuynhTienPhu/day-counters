03f99246bc6c2fce2998df7d507f37548663d952
DinhHuynhTienPhu <ff10111011@gmail.com>
2024-03-03 11:39:14 +0700

    i miss him btn

--- src/App.js
+++ src/App.js
@@ -1,71 +1,24 @@
-import hao from './hao.jpg';
-import './App.css';
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./home";
+import About from "./about";
 
 function App() {
-  var startDate = new Date(2024, 1, 27);
-  var today = new Date();
-  var difference = today - startDate;
-
-  var years = Math.floor(difference / (1000 * 60 * 60 * 24 * 365));
-  difference -= years * (1000 * 60 * 60 * 24 * 365);
-  var months = Math.floor(difference / (1000 * 60 * 60 * 24 * 30));
-  difference -= months * (1000 * 60 * 60 * 24 * 30);
-  var days = Math.floor(difference / (1000 * 60 * 60 * 24));
-  difference -= days * (1000 * 60 * 60 * 24);
-  var hours = Math.floor(difference / (1000 * 60 * 60));
-  difference -= hours * (1000 * 60 * 60);
-  var minutes = Math.floor(difference / (1000 * 60));
-  difference -= minutes * (1000 * 60);
-  var seconds = Math.floor(difference / 1000);
-
-
-  //then every 1 second, update the count
-  setInterval(function () {
-    var startDate = new Date(2024, 1, 27);
-    var today = new Date();
-    var difference = today - startDate;
-
-    var years = Math.floor(difference / (1000 * 60 * 60 * 24 * 365));
-    difference -= years * (1000 * 60 * 60 * 24 * 365);
-    var months = Math.floor(difference / (1000 * 60 * 60 * 24 * 30));
-    difference -= months * (1000 * 60 * 60 * 24 * 30);
-    var days = Math.floor(difference / (1000 * 60 * 60 * 24));
-    difference -= days * (1000 * 60 * 60 * 24);
-    var hours = Math.floor(difference / (1000 * 60 * 60));
-    difference -= hours * (1000 * 60 * 60);
-    var minutes = Math.floor(difference / (1000 * 60));
-    difference -= minutes * (1000 * 60);
-    var seconds = Math.floor(difference / 1000);
-
-    document.getElementById('text-count').textContent =
-      "It's been " + years + " years, " + months + " months, " + days + " days, " + hours + " hours, " + minutes + " minutes, and " + seconds + " seconds since he joined the military service!";
-    
-  }, 1000);
-
-
+ 
 
 
   return (
     // center this div in the middle of the page
 
-    <div className="App" >
-      <div  style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', margin:  '50px' }}>
-        <span id='text-count' >It's been {years} years, {months} months, {days} days, {hours} hours, {minutes} minutes, and {seconds} seconds since he joined the military service!</span>
-        <br />
-      </div>
-      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: '10px' }}>
-        <h1>We miss you, Hao</h1>
-      </div>
-      
-      <div>
-        <img src={hao} alt="hao" style={{ width: '300px' }} />
-      </div>
-      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: '90px' ,marginBottom:'10px' }}>
-        <a style={{ marginLeft: '30px' }} href="https://www.facebook.com/hachihao792001">>His facebook</a>
-        <a style={{ marginLeft: '30px' }} href="https://www.youtube.com/@BlueShadow792001/videos">>His legacy</a>
-        <a style={{ marginLeft: '30px' }} href="https://github.com/DinhHuynhTienPhu/day-counters">>Contribute to this project</a>
-      </div>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}>
+          {/* <Route path="*" element={<NoPage />} /> */}
+        </Route>
+        <Route path="/about" element={<About />} />
+
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/about.js
@@ -0,0 +1,33 @@
+export default function About() {
+    return (
+        <div className="About" style={{padding: '20px'}} >
+        <h1>Who is Hao</h1>
+        <p>
+                Hao is a friend of mine. He joined the military service in 2024 and has been serving since then. We miss him a lot and we hope he comes back soon.
+            </p>
+            <h2>Why did I make this website?</h2>
+            <p>
+                I made this website to keep track of how long it has been since he joined the military service. I hope he comes back soon.
+            </p>
+            <h2>Why did he get so much love from us</h2>
+            <p>
+                He is a very kind and caring person. He is always there for us when we need him. He is also very funny and makes us laugh all the time. 
+                <br />
+                Hao has a youtube channel where he shares his knowledge and experience. You can check it out <a href="https://www.youtube.com/@BlueShadow792001/videos">here</a>.
+                Hao is a game developer and he has a lot of cool projects. He has worked at VNG, a big game company in Vietnam for about 2 years.
+                <br />
+                We love him because he always want to share his knowledge and experience with us. He is a very good teacher and he is always there for us when we need him.
+                He is also very smart and a hard-working student. He is always the top of his class and he is always there to help us when we need him.
+                Some of his achievements:
+                <ul>
+                    <li>Got a scholarship for being top 5 in his class</li>
+                    <li>Graduated with GPA = 9.1 </li>
+                    <li>Many cool projects on his facebook. Including A* in minecraft, Image to maze,... <a href="https://www.facebook.com/hachihao792001">here</a></li>
+                </ul>
+            </p>
+
+            <br />
+            <a href="/">>Back Home</a>
+        </div>
+    );
+}
