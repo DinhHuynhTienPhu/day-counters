d493c6357547b2ce9e84d661cf16cb0b09e3eb14
DinhHuynhTienPhu <ff10111011@gmail.com>
2024-06-04 10:55:42 +0700

    theme garden

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -40,7 +40,7 @@ module.exports = {
       "nord",
       "sunset",
     ], // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
-    darkTheme: "cupcake", // name of one of the included themes for dark mode
+    darkTheme: "garden", // name of one of the included themes for dark mode
     base: true, // applies background color and foreground color for root element by default
     styled: true, // include daisyUI colors and design decisions for all components
     utils: true, // adds responsive and modifier utility classes
