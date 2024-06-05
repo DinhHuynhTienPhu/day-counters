d19684a3a3b1d4d77e6da8cd394148b00d89c00f
DinhHuynhTienPhu <ff10111011@gmail.com>
2024-06-05 10:22:45 +0700

    fix clock 100

--- src/components/CountDownClock.tsx
+++ src/components/CountDownClock.tsx
@@ -16,7 +16,7 @@ function CountDownClock() {
     const durationLabel = [ "day", "hour", "minute", "second"]
 
     const duration = moment.duration(time.diff("second"), "seconds");
-    const dayObject = useMemo(() => {
+    const   dayObject = useMemo(() => {
         const durationFormatted = duration.format("d [day],h [hour],m [min],s [sec]");
         const formatSplit = durationFormatted.split(",");
         return {
@@ -32,12 +32,17 @@ function CountDownClock() {
         <div className="grid grid-flow-col gap-5 text-center auto-cols-max">
             {durationLabel.map((key: string, _) => {
                 if (!dayObject[key]) return <></>;
-                const value = dayObject[key].split(" ")[0];
+                let value = dayObject[key].split(" ")[0];
                 const label = dayObject[key].split(" ")[1];
+
+                //if value is 1 character such as 1,2, change to 01,02
+                if (value.length === 1) {
+                    value = "0" + value;
+                }
                 return (
                     <div key={key} className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
-                        <span className="countdown font-mono text-5xl">
-                            <span style={{ "--value": value } as React.CSSProperties}></span>
+                        <span className="font-mono text-5xl">
+                            <span> {value} </span>
                         </span>
                         {label}
                     </div>
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,26 @@
 import CountDownClock from '../components/CountDownClock'
 import MissHim from '../components/MissHim'
+import { useEffect } from 'react'
+
+
 
 const HomePage = () => {
+  useEffect(() => {
+    console.log("I have been mounted")
+    try {
+      document.getElementById("backgroundMusic").play().catch((error) => {
+        
+        document.addEventListener('click', () => {
+          console.log("User has interacted with the page")
+          document.getElementById("backgroundMusic").play()
+        }, { once: true })
+      })
+    } catch (error) {
+      console.log("Error playing music", error)
+    }
+  }, [])
+
+
   return (
         <div className="hero min-h-[88vh] bg-base-200">
             <div className="hero-content flex-col lg:flex-row">
@@ -18,7 +37,7 @@ const HomePage = () => {
                 </div>
                 
             </div>
-          
+      <audio id="backgroundMusic" loop autoPlay src="./assets/music.mp3" />
         </div>
 
    
